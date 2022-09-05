556b2b32d1363495efff87e240727b7a988d97e8
Sivuyile Mene <smene98@gmail.com>
2022-09-05 19:54:24 +0200

    fixes issue3

--- .my_commands.sh
+++ .my_commands.sh
@@ -2,13 +2,18 @@
 
 function create () {
     source .env
-    cd
+    echo "creating remote repo...."
     python $FOLDERPATH/repo-automation/main.py $1
     cd $FOLDERPATH/$1
+    echo "initilizing local git....."
     git init
-    git remote add origin https://github.com/$USERNAME/$1.git
+    echo "creating README"
     touch README.md
     git add .
     git commit -m "Initial commit"
-    git push -u origin master
+    git branch -M main
+    echo "add remote...."
+    git remote add origin https://github.com/$USERNAME/$1.git
+    echo "pushing to remote..."
+    git push -u origin main
 }
